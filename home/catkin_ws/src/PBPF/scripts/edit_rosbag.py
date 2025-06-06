0015f3cb495bb67d1762f6dc208177cafabe5978
1210652636 <1210652636@qq.com>
2025-06-06 08:01:27 +0100

    update readme

--- home/catkin_ws/src/PBPF/scripts/edit_rosbag.py
+++ home/catkin_ws/src/PBPF/scripts/edit_rosbag.py
@@ -43,7 +43,7 @@ class ImageCreator():
 
 
 rosbag_file_path = os.path.expanduser("~/pyvkdepth/rosbag/")
-rosbag_file_name = "4_scene2_crackerParmesansoupMayo1_CP.bag"
+rosbag_file_name = "3_scene2_SaladDressingMustardMayo1.bag"
 save_file_path = os.path.expanduser("~/")
 if __name__ == '__main__':
     ImageCreator(rosbag_file_path+rosbag_file_name, save_file_path+"rgb/", save_file_path+"depth/", 1, 1)
